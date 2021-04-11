f20254ca8807108118f8ce3d2fc6524065670010
Thiarhes <injesusservice@hotmail.com>
2021-04-11 20:13:45 -0300

    navbar implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import { Switch, Route } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div style={divStyle}>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/' component={Login} />
+        </Switch>
       </div>
     )
   }
+}
+
+const divStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  textTransform: 'uppercase',
 }
---
+++ src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                <h2>SignUp</h2>
+            </div>
+        )
+    }
+}
